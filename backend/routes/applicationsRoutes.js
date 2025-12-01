7d17b809e89ffd9fe29b1f977e7c1fc3e0971a16
SravanthDev <sravanth7762@gmail.com>
2025-12-01 10:09:42 +0530

    updated routes

---
+++ backend/routes/applicationsRoutes.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {
+    applyForJob,
+    getUserApplications,
+    getJobApplications,
+    updateApplicationStatus
+} from '../controllers/applicationController.js';
+import { authenticate, authorize } from '../middlewares/authMiddleware.js';
+
+const router = express.Router();
+
+router.use(authenticate);
+
+router.post('/:jobId', authorize('JOB_SEEKER'), applyForJob);
+
+router.get('/user', authorize('JOB_SEEKER'), getUserApplications);
+
+router.get('/job/:jobId', authorize('RECRUITER'), getJobApplications);
+
+router.put('/:applicationId/status', authorize('RECRUITER'), updateApplicationStatus);
+
+export default router;
